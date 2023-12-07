c688c524e9c973e21c0e074bcebab03c56ab2a8c
manish <manishkumar60708090@gmail.com>
2023-12-07 14:31:42 +0530

    add multer

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,13 @@
 import express from "express"
 import { getProfile, login, signUp } from "../controllers/login.js"
+import { singleUpload } from "../middleware/multer.js"
 
 
 
 const router = express.Router()
 
 router.post('/login', login)
-router.post('/signUp', signUp)
+router.post('/signUp', singleUpload, signUp)
 router.get('/me', getProfile)
 
 
