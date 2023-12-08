73efb3bf695b60cea1fd03c56aac85baf9b288e4
manish <manishkumar60708090@gmail.com>
2023-12-08 14:42:10 +0530

    some change

--- controllers/user.js
+++ controllers/user.js
@@ -35,31 +35,63 @@ export const login = async(req, res, next) => {
 }
 
 
-export const signUp = async(req, res, next) => {
+// export const signUp = async(req, res, next) => {
 
-    const {name, email, password} = req.body;
+//     const {name, email, password} = req.body;
 
-    // avatar 
-    let avatar = undefined
-    if(req.file){
-        const file = getDataUri(req.file)
-        const myCloud = await cloudanary.v2.uploader.upload(file.content)
-        avatar = {
-            public_id:myCloud.public_id,
-            url:myCloud.secure_url,
-        }
-    }
+//     // avatar 
+//     let avatar = undefined
+//     if(req.file){
+//         const file = getDataUri(req.file)
+//         const myCloud = await cloudanary.v2.uploader.upload(file.content)
+//         avatar = {
+//             public_id:myCloud.public_id,
+//             url:myCloud.secure_url,
+//         }
+//     }
 
     
-    await User.create({
-        avatar,name,email,password
-    })
-    res.status(201).json({
-        success:true,
-        message:"Register Successfully"
-    })
-}
+//     await User.create({
+//         avatar,name,email,password
+//     })
+//     res.status(201).json({
+//         success:true,
+//         message:"Register Successfully"
+//     })
+// }
 
+
+export const signUp = async (req, res, next) => {
+    const { name, email, password } = req.body;
+  
+    
+  
+    
+  
+    let avatar = undefined;
+    if (req.file) {
+      const file = getDataUri(req.file);
+      const myCloud = await cloudanary.v2.uploader.upload(file.content);
+      avatar = {
+        public_id: myCloud.public_id,
+        url: myCloud.secure_url,
+      };
+    }
+  
+    user = await User.create({
+      // avatar,
+      name,
+      email,
+      password,
+      
+    });
+    res.status(400).json({
+      success:true,
+      message:'Register Successfully'
+    })
+  
+
+  }
 
 export const getProfile = (req, res, next) => {
     res.send('hello world')
