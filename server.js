277c2c141cd703b3bf074ad8889dcd4d7d07a288
manish <manishkumar60708090@gmail.com>
2023-12-09 22:14:10 +0530

    add double comma

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
 import { app } from "./app.js";
 import { connectDB } from "./data/database.js";
-// import cloudanary from 'cloudinary'
 import cloudinary from "cloudinary"
 
+// cloudinary.v2.config({
+//     cloud_name:process.env.CLOUDANARY_NAME,
+//     api_key:process.env.CLOUDANARY_API_KEY,
+//     api_secret:process.env.CLOUDANARY_API_SECRET,
+// })
+
+
 cloudinary.v2.config({
-    cloud_name:process.env.CLOUDANARY_NAME,
-    api_key:process.env.CLOUDANARY_API_KEY,
-    api_secret:process.env.CLOUDANARY_API_SECRET,
+    cloud_name:'dkyh6re1n',
+    api_key:'657991277378528',
+    api_secret:'mBnaAEfRTI522xI3QhqsU6Ekh-o',
 })
 
-// const signUpload = async () => {
-//     const timestamp = Math.round(newDate() /1000);
-//     const params = {
-//       timestamp: timestamp
-//     };
-//     const signature = await cloudinary.utils.api_sign_request(params, process.env.CLOUDANARY_API_SECRET);
-//     return { timestamp, signature };
-//   }
+
 
 
 app.listen(process.env.PORT, () => {
