2cf01eca8d45358356ff162c2b892ad286c06ced
manish <manishkumar60708090@gmail.com>
2023-12-09 19:35:39 +0530

    some cloudany prolem

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 import { app } from "./app.js";
 import { connectDB } from "./data/database.js";
-import cloudanary from 'cloudinary'
+// import cloudanary from 'cloudinary'
+import cloudinary from "cloudinary"
 
-
-cloudanary.v2.config({
+cloudinary.v2.config({
     cloud_name:process.env.CLOUDANARY_NAME,
     api_key:process.env.CLOUDANARY_API_KEY,
     api_secret:process.env.CLOUDANARY_API_SECRET,
 })
 
+// const signUpload = async () => {
+//     const timestamp = Math.round(newDate() /1000);
+//     const params = {
+//       timestamp: timestamp
+//     };
+//     const signature = await cloudinary.utils.api_sign_request(params, process.env.CLOUDINARY_SECRET);
+//     return { timestamp, signature };
+//   }
 
 
 app.listen(process.env.PORT, () => {
